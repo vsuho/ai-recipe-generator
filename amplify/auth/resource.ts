60453974968cac91160bc9760ac7371d21ee4f83
vsuho <156258471+vsuho@users.noreply.github.com>
2024-09-11 17:00:24 +0300

    connect to bedrock

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,15 @@
-import { defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
+
+
+import { defineAuth } from "@aws-amplify/backend";
+
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
+      verificationEmailBody: (createCode) =>
+        `Use this code to confirm your account: ${createCode()}`,
+    },
   },
 });
